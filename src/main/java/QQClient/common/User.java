b01e5bc26da3e435df43461b5bc177ce92cabed4
Kritu <2456768374@qq.com>
2024-02-05 16:06:31 +0800

    TCP通信服务端完成。这里编写的业务逻辑都集成在服务器中，真正的服务器往往只用来转发请求，具体的业务逻辑需要剥离出来交由框架处理

---
+++ src/main/java/QQClient/common/User.java
@@ -0,0 +1,22 @@
+package QQClient.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @Auther: Kritu
+ * @Date: 2024/2/3 18:09
+ * @Description:
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String userId;//用户Id/用户名
+    private String passwd;//用户密码
+
+}
